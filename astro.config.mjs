f3c223206e52d5dd2312525c1c785b091a327a97
cavb1205 <cavb.1205@hotmail.com>
2024-10-17 22:20:57 -0300

    add ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,13 @@ import tailwind from '@astrojs/tailwind';
 
 // import vercel from '@astrojs/vercel/serverless';
 // import vercel from '@astrojs/vercel';
-import vercel from "@astrojs/vercel/static";
+import vercel from '@astrojs/vercel/serverless';
 
 
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'static',
+  output: 'server',
   adapter: vercel(),
 });
